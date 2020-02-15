b63424bea5e2b9a1a3b4d91d233f41bdbd559041
alumno <alumno@utnfra.com.ar>
2020-02-14 21:21:24 -0300

    CursoIngresoJS

--- 8-TPs/jsFerreteIluminacion.js
+++ 8-TPs/jsFerreteIluminacion.js
@@ -10,77 +10,65 @@ E.	Si el importe final con descuento suma más de $120  se debe sumar un 10% de 
  */
 function CalcularPrecio () 
 {
- var preciolampara = 35
- var cantidad
- var marca
- var precioXcantidad
- var descuentoA
- var descuentoB
- var descuentoB2
- var descuentoC
- var descuentoC2
- var descuentoD
- var descuentoD2
- var descuentoD3
- cantidad = document.getElementById("Cantidad").value
- cantidad = parseInt(cantidad)
- precioXcantidad = (cantidad * preciolampara)
- marca = document.getElementById("Marca").value
- if (cantidad >= 6) {
-     descuentoA = (precioXcantidad - (precioXcantidad * 50 / 100))
-     document.getElementById("precioDescuento").value = descuentoA 
-    } 
-if (descuentoA > 120)  {
-     descuentoA = (descuentoA + (descuentoA * 10 / 100))
-     alert("Usted pago " + descuentoA)
-    } else if ((cantidad == 5) && (marca == "ArgentinaLuz"))  { 
-     descuentoB = (precioXcantidad - (precioXcantidad * 40 / 100)
-     document.getElementById("precioDescuento") = descuentoB 
-    }
-if (descuentoB > 120)  {
-        descuentoB = (descuentoB + (descuentoB * 10 / 100))
-        alert("Usted pago " + descuentoB)
-    } else if ((cantidad == 5) && (marca != "ArgentinaLuz") {
-     descuentoB2 = (precioXcantidad - (precioXcantidad * 30 / 100))
-     document.getElementById("precioDescuento").value = descuentoB2 
-    } 
-if (descuentoB2 > 120){
-        descuentoB2 = (descuentoB2 + (descuentoB2 * 10 / 100))
-        alert ("Usted pago " + descuentoB2)
-    } else if ((cantidad == 4) && (marca == ("ArgentinaLuz" || "FelipeLamparas"))){
-     descuentoC = (precioXcantidad - (precioXcantidad * 25 / 100))
-     document.getElementById("precioDescuento").value = descuentoC
-    } 
-if (descuentoC > 120) {
-        descuentoC = (descuentoC + (descuentoC * 10 / 100))
-        alert ("Usted pago " + descuentoC)
-    } else if ((cantidad == 4) && !(marca == ("ArgentinaLuz" || "FelipeLamparas"))){
-     descuentoC2 = (precioXcantidad - (precioXcantidad * 20 / 100))
-     document.getElementById("precioDescuento").value = descuentoC2
-    } 
-if (descuentoC2 > 120) {
-        descuentoC2 = (descuentoC2 + (descuentoC2 * 10 / 100))
-        alert ("Usted pago " + descuentoC2)
-    } else if ((cantidad == 3) && (marca == "ArgentinaLuz")){
-     descuentoD = (precioXcantidad - (precioXcantidad * 15 / 100))
-     document.getElementById("precioDescuento").value = descuentoD
-    }
-if (descuentoD > 120) {
-        descuentoD = (descuentoD + (descuentoD * 10 / 100)
-        alert ("Usted pago " + descuentoD)
-    } else if ((cantidad == 3) && (marca == "FelipeLamparas")){
-    descuentoD2 = (precioXcantidad - (precioXcantidad * 10 / 100))
-    document.getElementById("precioDescuento").value = descuentoD2 
-    }
-if (descuentoD2 > 120) {
-    descuentoD2 = (descuentoD2 + (descuentoD2 * 10 / 100)
-    alert("Usted pago " + descuentoD2)
-    } else if ((cantidad == 3) && (marca != ("FelipeLamparas" || "ArgentinaLuz")){
-    descuentoD3 = (precioXcantidad - (precioXcantidad * 5 / 100))
-    document.getElementById("precioDescuento").value = descuentoD3
-    }
-if (descuentoD3 > 120) {
-    descuentoD3 = (descuentoD3 + (descuentoD3 * 10 / 100))
-    alert("Usted pago " + descuentoD3)
-    }
+var cantidad = document.getElementById("Cantidad").value
+var marca = document.getElementById("Marca").value
+var precioxcant = cantidad * 35
+var descuento
+var impuesto
+cantidad = parseInt(cantidad)
+if (cantidad >= 6){
+    descuento = (precioxcant + (precioxcant * 50 / 100))
+    document.getElementById("precioDescuento").value = descuento
+} else if (descuento > 120) {
+    impuesto = (descuento * 10) / 100
+    alert("Usted pago " + impuesto)
+}
+switch (marca)
+{
+    case "ArgentinaLuz":
+        if (cantidad == 5) {
+            descuento = (precioxcant + (precioxcant * 40 / 100))
+        } else if (cantidad == 4) {
+            descuento = (precioxcant + (precioxcant * 25 / 100))
+        } else if (cantidad == 3){
+            descuento = precioxcant + (precioxcant * 15 / 100)
+        }
+        if (descuento > 120) {
+            impuesto = (descuento * 10 / 100)
+            document.getElementById("precioDescuento").value = "Precio con descuento: " + descuento + " y pago de impuestos: " + impuesto
+        } else {
+            document.getElementById("precioDescuento").value = descuento
+        }
+        break;
+   case "FelipeLamparas":
+        if (cantidad == 5) {
+        descuento = (precioxcant + (precioxcant * 30 / 100))
+        } else if (cantidad == 4) {
+            descuento = (precioxcant + (precioxcant * 25 / 100))
+        } else if (cantidad == 3) {
+            descuento = (precioxcant + (precioxcant * 10 / 100))
+        }
+        if (descuento > 120) {
+            impuesto = (descuento * 10 / 100)
+            document.getElementById("precioDescuento").value = "Precio con descuento: " + descuento + " y pago de impuestos: " + impuesto
+        } else {
+            document.getElementById("precioDescuento").value = descuento
+        }
+        break;
+    default:
+        if (cantidad == 5) {
+            descuento = (precioxcant + (precioxcant * 30 / 100))
+        } else if (cantidad == 4) {
+            descuento = (precioxcant + (precioxcant * 20 / 100))
+        } else if (cantidad == 3){
+            descuento = precioxcant + (precioxcant * 5 / 100)
+        }
+        if (descuento > 120) {
+            impuesto = (descuento * 10 / 100)
+            document.getElementById("precioDescuento").value = "Precio con descuento: " + descuento + " y pago de impuestos: " + impuesto
+        } else {
+            document.getElementById("precioDescuento").value = descuento
+        }
+        break;
+}
 }
--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/seis.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/seis.js
@@ -1,4 +1,41 @@
 function mostrar()
 {
-
+var hora
+hora = document.getElementById("laHora").value
+hora = parseInt(hora)
+switch (hora)
+{
+    case 6:
+    case 7:
+    case 8:
+    case 9:
+    case 10:
+    case 11:
+        alert("Es de mañana")
+        break;
+    case 12:
+    case 13:
+    case 14:
+    case 15:
+    case 16:
+    case 17:
+    case 18:
+    case 19:
+        alert("Es de tarde")
+        break;
+    case 20:
+    case 21:
+    case 22:
+    case 23:
+    case 24:
+        if (hora != 24)
+        {
+        alert("A mimir")
+        } else {
+        alert("Es de noche")
+        }
+        break;
+    default:
+        alert("No es una hora valida")
+}
 }
